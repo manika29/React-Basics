7257fa370101ecb64a77a5992772176b1b367012
Manika Sharma <49314189+manika29@users.noreply.github.com>
2022-01-13 01:14:36 +0530

    practice

--- Modern JS/index.js
+++ Modern JS/index.js
@@ -1,5 +1,24 @@
-const obj = {
-  name: "Manika",
-};
-obj.name = "zen";
-console.log(obj.name);
+//*****const keyword*****
+// const obj = {
+//   name: "Manika",
+// };
+// obj.name = "zen";
+// console.log(obj.name);
+
+// *****Template String*****
+// let fnmae = "Manika";
+// let lname = "Sharma";
+// let age = prompt("Enter your age");
+
+// let result = `${fnmae} ${lname} is ${age} years old`;
+// alert(result);
+
+// *****Functions*****
+// function welcome(user) {
+//   alert(`Hello ${user}`);
+// }
+// welcome("Manika");
+
+// *****Arrow Functions*****
+// let greeting = (message) => alert(`${message}`);
+// greeting("Hello");
